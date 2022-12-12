4f882b66cf9bc7b545ba445617c3f4fe004a82c9
Reuben Danu Arbito <79549355+ReubenDanu@users.noreply.github.com>
2022-12-13 06:58:45 +0700

    commit

--- db/connection.php
+++ db/connection.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost","root","","dbtokobuku");
+    $con = mysqli_connect("localhost","root","password","dbtokobuku");
 
     if(mysqli_connect_errno()){
         echo "Gagal untuk terkoneksi ke database " . mysqli_connect_error();
